a803300eecf75ee65902a7a9de85b56f7cb2819b
adhakad <72352125+adhakad@users.noreply.github.com>
2022-07-28 16:59:47 +0530

    Update classTeacher.js

--- modules/classTeacher.js
+++ modules/classTeacher.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://pms-demo:@Aa1Bb2Hh3@@cluster0.ngu0t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology:true,useFindAndModify:false,});
+mongoose.connect('mongodb+srv://adminUser:7pH4LDTnmlLMsCSA@@cluster0.ngu0t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology:true,useFindAndModify:false,});
 var conn =mongoose.Collection;
 var classTeacherSchema =new mongoose.Schema({
     teachername:{type:String,
